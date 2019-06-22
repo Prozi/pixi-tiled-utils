f15a6f74be6fa9af13827f4b9e5fe31b8a6b0c8e
Jacek Pietal <jacek@gamecodehq.com>
2019-06-22 10:29:39 +0200

    build with webpack 1.0.1

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    target: 'web',
+    entry: './lib/index.js',
+    output: {
+        path: require('path').resolve(__dirname),
+        filename: 'index.js',
+        library: 'pixi-tiled-utils',
+        libraryTarget: 'commonjs'
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    optimization: {
+        minimize: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['@babel/preset-env'],
+                }
+            }
+        ]
+    }
+}
